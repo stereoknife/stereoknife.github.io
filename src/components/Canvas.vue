707032be7a4b58c27d37799c4b17bbb96873f216
kam <ignasi.gv@outlook.es>
2019-09-18 21:46:47 +0300

    Add Canvas component

---
+++ src/components/Canvas.vue
@@ -0,0 +1,29 @@
+<template>
+  <canvas
+    class="w-screen h-screen"
+    ref="canvas"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Canvas',
+  data () {
+    return {
+      ctx: null
+    }
+  },
+  computed: {
+    cWidth: function () { return this.$refs.canvas.width },
+    cHeight: function () { return this.$refs.canvas.height },
+    cMidX: function () { return this.$refs.canvas.width * 0.5 },
+    cMidY: function () { return this.$refs.canvas.height * 0.5 }
+  },
+  mounted () {
+    const canvas = this.$refs.canvas
+    this.ctx = canvas.getContext('2d')
+    canvas.height = canvas.clientHeight
+    canvas.width = canvas.clientWidth
+  }
+}
+</script>
